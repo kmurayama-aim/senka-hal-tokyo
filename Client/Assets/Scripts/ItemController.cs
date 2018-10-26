fc549b7ccff95c31ff1ff9b60bd0b7b62805fc7e
kmurayama <kmurayama@aiming-inc.com>
2018-10-26 10:36:52 +0900

    使用していなかった変数を削除

--- Client/Assets/Scripts/ItemController.cs
+++ Client/Assets/Scripts/ItemController.cs
@@ -7,11 +7,9 @@ public class ItemController : MonoBehaviour
     Rigidbody rb;
     public event Action OnGet;
     public event Action<Rigidbody> MoveToPlayer;
-    MainController mainControllerSc;
 
     void Awake()
     {
-        mainControllerSc = GameObject.Find("Main").GetComponent<MainController>();
     }
 
     private void FixedUpdate()
