1873673446851a6aacaa6184f40f9d6d35d2c5e5
kmurayama <kmurayama@aiming-inc.com>
2019-01-09 15:04:12 +0900

    [リファクタリング] 別のデータに整形するクラスなので、Formatの方が正しいと判断したため変更
    参考記事：
    https://qiita.com/KeithYokoma/items/ee21fec6a3ebb5d1e9a8#%E3%83%87%E3%83%BC%E3%82%BF%E3%82%92%E5%8A%A0%E5%B7%A5%E3%81%99%E3%82%8B%E3%83%AC%E3%82%A4%E3%83%A4
    参考部分のGyazo：
    https://gyazo.aiming-inc.biz/data/0/7/071976c077007eac20e19a39336bb425.png

--- Client/Assets/Scripts/JsonMessageFormatter.cs
+++ Client/Assets/Scripts/JsonMessageFormatter.cs
@@ -3,26 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using RPC = WebSocketSample.RPC;
 
-public class JsonMessageCreater
+public class JsonMessageFormatter
 {
-    public string CreateLoginMessage(string message)
+    public string FormatLoginMessage(string message)
     {
         var jsonMessage = JsonUtility.ToJson(new RPC.Login(new RPC.LoginPayload(message)));
         return jsonMessage;
     }
-    public string CreateCollisionMessage(int playerId, int otherPlayerId)
+    public string FormatCollisionMessage(int playerId, int otherPlayerId)
     {
         var collisionRpc = new RPC.Collision(new RPC.CollisionPayload(playerId, otherPlayerId));
         var jsonMessage = JsonUtility.ToJson(collisionRpc);
         return jsonMessage;
     }
-    public string CreatePlayerUpdateMessage(Vector3 pos, int playerId)
+    public string FormatPlayerUpdateMessage(Vector3 pos, int playerId)
     {
         var rpcPosition = new RPC.Position(pos.x, pos.y, pos.z);
         var jsonMessage = JsonUtility.ToJson(new RPC.PlayerUpdate(new RPC.PlayerUpdatePayload(playerId, rpcPosition)));
         return jsonMessage;
     }
-    public string CreateGetItemMessage(int itemId, int playerId)
+    public string FormatGetItemMessage(int itemId, int playerId)
     {
         var getItemRpc = new RPC.GetItem(new RPC.GetItemPayload(itemId, playerId));
         var jsonMessage = JsonUtility.ToJson(getItemRpc);
--- Client/Assets/Scripts/MessageSenderToServer.cs
+++ Client/Assets/Scripts/MessageSenderToServer.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class MessageSenderToServer
 {
     WebSocketInitializer socketInitializer = new WebSocketInitializer();
-    JsonMessageCreater jsonMessageCreater = new JsonMessageCreater();
+    JsonMessageFormatter jsonMessageFormatter = new JsonMessageFormatter();
 
     public void SendLoginMessage(string connectAddress, string playerName)
     {
         socketInitializer.Initialize(connectAddress);
 
-        var jsonMessage = jsonMessageCreater.CreateLoginMessage(playerName);
+        var jsonMessage = jsonMessageFormatter.FormatLoginMessage(playerName);
         Debug.Log(jsonMessage);
 
         socketInitializer.Send(jsonMessage);
@@ -20,20 +20,20 @@ public class MessageSenderToServer
 
     public void SendPlayerOnCollisionMessage(int playerId, int otherPlayerId)
     {
-        var collisionJson = jsonMessageCreater.CreateCollisionMessage(playerId, otherPlayerId);
+        var collisionJson = jsonMessageFormatter.FormatCollisionMessage(playerId, otherPlayerId);
         socketInitializer.Send(collisionJson);
     }
 
     public void SendUpdatePositionMessage(Vector3 currentPlayerPosition, int playerId)
     {
-        var jsonMessage = jsonMessageCreater.CreatePlayerUpdateMessage(currentPlayerPosition, playerId);
+        var jsonMessage = jsonMessageFormatter.FormatPlayerUpdateMessage(currentPlayerPosition, playerId);
         Debug.Log(jsonMessage);
         socketInitializer.Send(jsonMessage);
     }
 
     public void SendItemOnGetMessage(int itemId, int playerId)
     {
-        var getItemJson = jsonMessageCreater.CreateGetItemMessage(itemId, playerId);
+        var getItemJson = jsonMessageFormatter.FormatGetItemMessage(itemId, playerId);
         socketInitializer.Send(getItemJson);
         Debug.Log(">> GetItem");
     }
