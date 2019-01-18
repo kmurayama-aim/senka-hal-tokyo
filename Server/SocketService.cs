830c67ae238d848043d8810e6c2c8dc36c5b96fc
kmurayama <kmurayama@aiming-inc.com>
2019-01-18 17:23:02 +0900

    [リファクタリング] WebSocketとサーバーをつなぐためのクラスを作成
    WebSocketとServerのメッセージの送受信を仲介する
    RPCやJson、依存関係などの情報をServerに侵入させないように隔離する腐敗防止層の役割

---
+++ Server/SocketService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Newtonsoft.Json;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+using WebSocketSample.RPC;
+
+namespace WebSocketSample.Server
+{
+    class SocketService : WebSocketBehavior
+    {
+        GameService sendMessageTarget;
+
+        public SocketService(GameService sendMessageTarget)
+        {
+            this.sendMessageTarget = sendMessageTarget;
+        }
+
+        protected override void OnOpen()
+        {
+        }
+
+        protected override void OnClose(CloseEventArgs e)
+        {
+        }
+
+        protected override void OnMessage(MessageEventArgs e)
+        {
+        }
+
+        protected override void OnError(ErrorEventArgs e)
+        {
+        }
+    }
+}
